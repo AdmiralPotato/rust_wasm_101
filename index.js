d22a9137d2f901353bbca8a148227af045bc7744
Admiral Potato <admiral@nuclearpixel.com>
2023-09-07 14:53:04 -0600

    Start parsing some more complex input

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import init, {add, div, goat_song} from './pkg/rust_wasm_101.js';
+import init, {
+  add,
+  div,
+  goat_song,
+  parse_goat_log
+} from './pkg/rust_wasm_101.js';
 
 await init();
 
@@ -10,3 +15,42 @@ console.log('What is divideResult?', divideResult);
 
 const goatSongResult = goat_song(5);
 console.log('What is goatSongResult?', goatSongResult);
+
+const goatLog = `
+name: Gruff
+power_level: 100
+is_grumpy: true
+
+name: Faun
+power_level: 9001
+is_grumpy: false
+
+name: Billy
+power_level: 100
+is_grumpy: true
+`;
+
+const parsedGoats = parse_goat_log(goatLog);
+console.log('What is parsedGoats?', parsedGoats);
+
+
+const sillyGoatLog = `
+power_level: 1001
+is_grumpy: true
+name: Backwards Goat
+
+power_level: 900100
+power_level: 10
+is_grumpy: false
+is_grumpy: true
+is_grumpy: false
+is_grumpy: true
+name: Timmy the indecisive destroyer
+
+power_level: 100
+name: Flower
+is_grumpy: false
+`;
+
+const parsedSillyGoats = parse_goat_log(sillyGoatLog);
+console.log('What is parsedSillyGoats?', parsedSillyGoats);
--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,64 @@ pub fn goat_song(count: i32) -> String {
     }
     return result.to_string();
 }
+
+#[derive(Debug)]
+pub struct Goat {
+    pub name: String,
+    pub power_level: i32,
+    pub is_grumpy: bool
+}
+impl Goat {
+    pub fn parse_log(goat_log: String) -> Vec<Goat> {
+        let mut result = vec!();
+        let lines = goat_log.trim().split("\n");
+        let mut name_opt: Option<String> = None;
+        let mut power_level_opt: Option<i32> = None;
+        let mut is_grumpy_opt: Option<bool> = None;
+        for line in lines {
+            let mut line = line.trim().split(':');
+            let key = line.next();
+            let value = line.next();
+            // TODO: Implement log output on the JS side?
+            println!("key: '{key:?}', value: '{value:?}'");
+            match (key, value) {
+                (Some(key), Some(value)) => {
+                    let value = value.trim();
+                    match key.trim() {
+                        "name" => name_opt = Some(value.to_string()),
+                        "power_level" => power_level_opt = Some(value.parse::<i32>().expect("bad number")),
+                        "is_grumpy" => is_grumpy_opt = match value {
+                            "true" => Some(true),
+                            "false" => Some(false),
+                            _ => None
+                        },
+                        _ => println!("Invalid key")
+                    }
+                    match(&name_opt, &power_level_opt, &is_grumpy_opt) {
+                        (Some(name), Some(power_level), Some(is_grumpy)) => {
+                            println!("Goat is ready!");
+                            result.push(Goat{
+                                name: name.clone(),
+                                power_level: *power_level,
+                                is_grumpy: *is_grumpy
+                            });
+                            name_opt = None;
+                            power_level_opt = None;
+                            is_grumpy_opt = None;
+                        },
+                        _ => println!("Goat not ready yet")
+                    }
+                },
+                _ => println!("Missing key or value")
+            }
+        }
+        result
+    }
+}
+
+#[wasm_bindgen]
+pub fn parse_goat_log(goat_log: String) -> String {
+    // TODO: Return Vec<Goat>
+    let result = Goat::parse_log(goat_log);
+    format!("Debug! {result:?}")
+}
